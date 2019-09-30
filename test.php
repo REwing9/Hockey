0d59a9025790318dce9e6f1f90b914e6c9d67c10
Rashaun Ewing <rewing9@gmail.com>
2019-09-30 15:58:42 -0500

    Added roster

--- test.php
+++ test.php
@@ -1,5 +1,26 @@
 <?php
-
-echo 'Initial message'; 
+$roster = array(
+  'Barker',
+  'Bodine',
+  'Jens',
+  'Gavin C.',
+  'Owen',
+  'Jonah',
+  'Peter',
+  'Greco',
+  'Gavin',
+  'Kendo',
+  'Joe',
+  'Moffit',
+  'Rory',
+  'Karlo',
+  'Shane',
+  'Charlie',
+  'Val',
+  'Will',
+  'Wags',
+);
+//echo 'Initial message';
 
  ?>
+ 
